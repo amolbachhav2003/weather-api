e75e53c80e685c2b8e867bfe90c957cec9df4b3a
Amol Bachhav <127419663+amolbachhav2003@users.noreply.github.com>
2024-10-22 22:01:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+
+  const API_KEY = "5938991ca3585919457c1147d4370f6d";
+
+  getWeatherBtn,addEventListener("click", async() => {
+      const cityName = cityInput.value.trim();
+      cityInput.value = "";
+      if(!cityName) return;
+
+      try {
+        const cityDetails = await fetchCityDetails(cityName);
+        displayCityDetails(cityDetails);
+      } catch (error) {
+        showError();
+      }
+      
+  })
+
+  async function fetchCityDetails(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    
+    const response = await fetch(url);
+
+    if(!response.ok){
+      throw new Error("City not found");
+    }
+
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+    
+    return data;
+  }
+
+  function displayCityDetails(cityDetails) {
+
+    const {name, main, weather} = cityDetails;
+
+    weatherInfo.classList.remove("hidden")
+
+    cityNameDisplay.textContent = `City: ${name}`;
+    temperatureDisplay.textContent = `Temperature: ${main.temp}`;
+    descriptionDisplay.textContent = `Weather: ${weather[0].main}`;
+     
+  }
+
+  function showError() {
+    errorMessage.classList.remove("hidden");
+    weatherInfo.classList.add("hidden");
+  }
+
+})
+
